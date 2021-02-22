a0eb0e915bb5e2206ec9b49cd7c7767ff1886e1f
AKSHAY GOYAL <a49goyal@uwaterloo.ca>
2021-02-22 02:55:52 -0500

    Created API and View

--- tinyURLapp/shortLogic.py
+++ tinyURLapp/shortLogic.py
@@ -1,11 +1,46 @@
-import random
-import string
+# import random
+# import string
+
+# class Shortner:
+#     token_size = 5
+#     def __init__(self, token_size = None):
+#         self.token_size = token_size if token_size is not None else 5
+
+#     def issue_token(self):
+#         letters = string.ascii_letters
+#         return ''.join(random.choice(letters) for i in range(self.token_size))
+
+####################################
+####BAse 10 to Base 64 Conversion###
+####################################
 
 class Shortner:
-    token_size = 5
-    def __init__(self, token_size = None):
-        self.token_size = token_size if token_size is not None else 5
+    url_mapper = {}
+    url_id = 1000000
 
-    def issue_token(self):
-        letters = string.ascii_letters
-        return ''.join(random.choice(letters) for i in range(self.token_size))
+    def shorten_url(self, long_url):
+        if long_url in self.url_mapper:
+            # print("ALREADY PRESENT")
+            id = self.url_mapper[long_url]
+            short_url = self.tokenGenerator(id)
+        else:
+            # print("CREATING NEW")
+            self.url_mapper[long_url] = self.url_id 
+            short_url = self.tokenGenerator(self.url_id)
+            self.url_id += 1
+        
+        return str(short_url)
+    
+    def tokenGenerator(self, id):
+        token = []
+        base64_chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        base64_length = len(base64_chars)
+
+        while id > 0:
+            idx = id % base64_length
+            token.append(base64_chars[idx])
+            id = id // base64_length
+        
+        short_token = "".join(token[::-1])
+        
+        return short_token
--- tinyURLapp/views.py
+++ tinyURLapp/views.py
@@ -3,3 +3,38 @@ from django.contrib import messages ## for Flashing Messages
 
 from .models import short_url
 from .forms import URLForm
+from .shortLogic import Shortner
+
+Shortner_obj = Shortner()
+
+## Home Page
+def home(request):
+
+    ## Base URL for Tiny URL's
+    BASE_URL = request.get_raw_uri()
+
+    hash_value = ''
+
+    if request.method == 'POST':
+        form = URLForm(request.POST)
+        if form.is_valid():
+            form_details = form.save()
+            original_url = form.cleaned_data.get('long_url') ## fetch user provided long URL
+            hash_value = Shortner_obj.shorten_url(str(original_url))
+            form_details.short_url = hash_value
+            form_details.save()
+
+            messages.success(request, f'Long URL converted to following Short URL:\n{BASE_URL}{hash_value}')
+            return redirect('tiny-home')
+    else:
+        form = URLForm()
+    
+    return render(request, 'tinyURLapp/home.html', {'form': form})
+
+## Redirect Page (i.e. to Original URL Page)
+def divert(request, hash_value):
+    url_obj = short_url.objects.filter(short_url=hash_value)[0]
+    return redirect(url_obj.long_url) 
+
+
+
