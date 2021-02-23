5533251525e9c70e4ec61a8a2316b4bbbf0971be
Baz07 <67526825+Baz07@users.noreply.github.com>
2021-02-22 22:13:23 -0500

    Updated Logic to generate Short URL (Used SHA512 and Base 64 Encoding)

--- tinyURLapp/shortLogic.py
+++ tinyURLapp/shortLogic.py
@@ -18,11 +18,11 @@ class Shortner:
 #         return str(short_url)
     
     def shorten_url(self, long_url, char_length = 8):
-        hash_obj = hashlib.sha512(long_url.encode())
-        hash_hexval = hash_obj.hexdigest()
-        hash_int = int(hash_hexval, 16)
-        short_url = self.tokenGenerator(hash_int)
-        return short_url[0:char_length]
+        hash_obj = hashlib.sha512(long_url.encode()) ## Calculate SHA512 Hash
+        hash_hexval = hash_obj.hexdigest() ## Convert the Hash value to Hex value
+        hash_int = int(hash_hexval, 16) ## Convert it to int base for encoding purposes
+        short_url = self.tokenGenerator(hash_int) ## Pass it down to token Generator to Base 64 conversion
+        return short_url[0:char_length] ## Return first 8 characters of short URL
     
    
     ## Base 10 to Base 64 Conversion Logic
